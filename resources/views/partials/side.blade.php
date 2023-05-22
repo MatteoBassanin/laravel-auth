be55e811fd7493a8d7b4817e32ac5e920452d03c
MatteoBassanin <matteo.bassanin@gmail.com>
2023-05-22 16:53:44 +0200

    added migrations working on faker

--- resources/views/partials/side.blade.php
+++ resources/views/partials/side.blade.php
@@ -3,19 +3,12 @@
         <svg class="bi pe-none me-2" width="40" height="32">
             <use xlink:href="#bootstrap"></use>
         </svg>
-        <span class="fs-4">Sidebar</span>
     </a>
-    <hr>
     <ul class="nav nav-pills flex-column mb-auto">
         <li class="nav-item">
             <a href="#" class="nav-link active" aria-current="page">
-                <svg class="bi pe-none me-2" width="16" height="16">
-                    <use xlink:href="#home"></use>
-                </svg>
-                Home
+                <div class="text-center">Dashboard</div>
             </a>
         </li>
     </ul>
-    <hr>
-
 </div>
